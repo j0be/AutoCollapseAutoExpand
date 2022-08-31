c117441f9f7a8a3d8d7742fdab6da20b70b3971a
Ronald Troyer <ronald.troyer@chase.com>
2022-08-31 14:42:17 -0400

    Handle move and attach scenarios

--- background.js
+++ background.js
@@ -1,27 +1,42 @@
 let tabGroupActiveTabIdCache = {};
 
-chrome.tabs.onActivated.addListener(activeInfo => {
-    chrome.tabs.get(activeInfo.tabId).then(tab => {
+function collapseTabGroup(tabGroupId) {
+    chrome.tabGroups.update(tabGroupId, { collapsed: true })
+        //This makes sure we have access to collapse: "Tabs cannot be edited right now (user may be dragging a tab"
+        .catch(() => { return setTimeout(collapseTabGroup.bind(this, tabGroupId), 100); });
+}
+
+function handleTabChange(tabId) {
+    chrome.tabs.get(tabId).then(tab => {
         if (tab.groupId > -1) {
-            tabGroupActiveTabIdCache[tab.groupId] = activeInfo.tabId;
+            tabGroupActiveTabIdCache[tab.groupId] = tabId;
         }
 
         chrome.tabGroups.query({}, tabGroups => {
             tabGroups.filter(tabGroup => {
                 return tabGroup.id !== tab.groupId;
             }).forEach(tabGroup => {
-                setTimeout(() => { chrome.tabGroups.update(tabGroup.id, { collapsed: true }); }, 100);
+                collapseTabGroup(tabGroup.id);
             });
         });
     });
-});
+}
 
-chrome.tabGroups.onUpdated.addListener(updatedTab => {
-    if (!updatedTab.collapsed && tabGroupActiveTabIdCache[updatedTab.id]) {
-        chrome.tabs.get(updatedTab.id, function(tab) {
-            chrome.tabs.highlight({ 'tabs': tab.index }, function(data) {
-                console.log('updated', data);
-            });
-        });
+chrome.tabs.onActivated.addListener(activeInfo => handleTabChange(activeInfo.tabId));
+chrome.tabs.onAttached.addListener(tabId => handleTabChange(tabId));
+chrome.tabs.onMoved.addListener(tabId => handleTabChange(tabId));
+
+chrome.tabGroups.onUpdated.addListener(async (updatedTabGroup) => {
+    if (!updatedTabGroup.collapsed) {
+        let tabId = tabGroupActiveTabIdCache[updatedTabGroup.id];
+        if (!tabId) {
+            await chrome.tabs.query({ groupId: updatedTabGroup.id })
+                .then(tabs => {
+                    tabId = tabs.sort((a, b) => String(a.index).localeCompare(String(b.index), { numeric: true }))[0].id;
+                });
+        }
+
+        chrome.tabs.get(tabId)
+            .then(tab => chrome.tabs.highlight({ 'tabs': tab.index }), () => {});
     }
 });
