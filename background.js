5460bd93dcf09a9dc85d1eda8ff3f3a9707f56fa
Ronald Troyer <ronald.troyer@chase.com>
2022-09-26 11:09:27 -0400

    Make sure to wait for any other extensions tab management

--- background.js
+++ background.js
@@ -1,36 +1,36 @@
 let tabGroupActiveTabIdCache = {};
 function throttle (func, wait = 100) {
-    let timer = null;
-    return function (...args) {
-        if (timer === null) {
-            timer = setTimeout(() => {
-                func.apply(this, args);
-                timer = null;
-            }, wait);
-        }
-    };
+  let timer = null;
+  return function (...args) {
+    if (timer === null) {
+      timer = setTimeout(() => {
+        func.apply(this, args);
+        timer = null;
+      }, wait);
+    }
+  };
 }
 
-function collapseTabGroup(tabGroupId) {
-    chrome.tabGroups.update(tabGroupId, { collapsed: true })
-        //This makes sure we have access to collapse: "Tabs cannot be edited right now (user may be dragging a tab"
-        .catch(() => { return setTimeout(collapseTabGroup.bind(this, tabGroupId), 100); });
+function collapseTabGroup (tabGroupId) {
+  chrome.tabGroups.update(tabGroupId, { collapsed: true })
+    //This makes sure we have access to collapse: "Tabs cannot be edited right now (user may be dragging a tab"
+    .catch(() => { return setTimeout(collapseTabGroup.bind(this, tabGroupId), 100); });
 }
 
-function handleTabChange(tabId) {
-    chrome.tabs.get(tabId).then(tab => {
-        if (tab.groupId > -1) {
-            tabGroupActiveTabIdCache[tab.groupId] = tabId;
-        }
+function handleTabChange (tabId) {
+  chrome.tabs.get(tabId).then(tab => {
+    if (tab.groupId > -1) {
+      tabGroupActiveTabIdCache[tab.groupId] = tabId;
+    }
 
-        chrome.tabGroups.query({}, tabGroups => {
-            tabGroups.filter(tabGroup => {
-                return tabGroup.id !== tab.groupId;
-            }).forEach(tabGroup => {
-                collapseTabGroup(tabGroup.id);
-            });
-        });
+    chrome.tabGroups.query({}, tabGroups => {
+      tabGroups.filter(tabGroup => {
+        return tabGroup.id !== tab.groupId;
+      }).forEach(tabGroup => {
+        collapseTabGroup(tabGroup.id);
+      });
     });
+  });
 }
 
 let tabThrottler = throttle(handleTabChange, 250);
@@ -40,16 +40,21 @@ chrome.tabs.onAttached.addListener(tabId => tabThrottler(tabId));
 chrome.tabs.onMoved.addListener(tabId => tabThrottler(tabId));
 
 chrome.tabGroups.onUpdated.addListener(async (updatedTabGroup) => {
-    if (!updatedTabGroup.collapsed) {
-        let tabId = tabGroupActiveTabIdCache[updatedTabGroup.id];
-        if (!tabId) {
-            await chrome.tabs.query({ groupId: updatedTabGroup.id })
-                .then(tabs => {
-                    tabId = tabs.sort((a, b) => String(a.index).localeCompare(String(b.index), { numeric: true }))[0].id;
-                });
-        }
+  setTimeout((async (updatedTabGroup) => {
+    let currentTab = await chrome.tabs.query({ active: true, lastFocusedWindow: true }).catch(() => {}); //Swallow
+    if (!updatedTabGroup.collapsed && currentTab.groupId !== updatedTabGroup.id) {
+      let tabId = tabGroupActiveTabIdCache[updatedTabGroup.id];
+      if (!tabId) {
+        await chrome.tabs.query({ groupId: updatedTabGroup.id })
+          .catch(() => {}) //Swallow
+          .then(tabs => {
+            tabId = tabs.sort((a, b) => String(a.index).localeCompare(String(b.index), { numeric: true }))[0].id;
+          });
+      }
 
-        chrome.tabs.get(tabId)
-            .then(tab => chrome.tabs.highlight({ 'tabs': tab.index }), () => {});
+      chrome.tabs.get(tabId)
+        .catch(() => {}) //Swallow
+        .then(tab => chrome.tabs.highlight({ 'tabs': tab.index }), () => {});
     }
+  }).bind(null, updatedTabGroup), 250);
 });
